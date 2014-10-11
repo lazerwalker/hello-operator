abc1d61c0e9e61899f2fd72a3488d4573f4d305f
Mike Walker <michael@lazerwalker.com>
2014-10-11 12:40:07 -0400

    Arduino script sends JSON

---
+++ game/game.ino
@@ -0,0 +1,54 @@
+int LOW_PIN = 10;
+int HIGH_PIN = 13;
+int NUMBER_OF_PINS = HIGH_PIN - LOW_PIN + 1;
+
+int STATE_UNUSED = -1;
+
+int state[20];
+
+String onJSON(int a, int b) {
+  return "{\"on\": [" + String(a) + "," + String(b) + "]}";
+}
+
+String offJSON(int a, int b) {
+  return "{\"off\": [" + String(a) + "," + String(b) + "]}";
+}
+
+void setup() {
+  Serial.begin(9600);
+  for (int i=LOW_PIN; i<=HIGH_PIN; i++) {
+    pinMode(i, INPUT_PULLUP);
+    
+    state[i] = STATE_UNUSED;
+  }
+}
+
+void loop() {
+  for (int i=LOW_PIN; i<=HIGH_PIN; i++) {
+    pinMode(i, OUTPUT);
+    digitalWrite(i, LOW);  
+      
+    for (int j=LOW_PIN; j<=HIGH_PIN; j++) {
+      if (i == j) { continue; }
+      
+      int val = digitalRead(j);
+      
+      if (val == LOW) {
+        if (state[j] == STATE_UNUSED) {
+          Serial.println(onJSON(i, j));
+        }
+        state[j] = i;
+        state[i] = j;
+        
+      } else if (val == HIGH) {
+        if (state[j] != STATE_UNUSED && state[i] != STATE_UNUSED) {
+          Serial.println(offJSON(i, j));
+          state[i] = STATE_UNUSED;
+          state[j] = STATE_UNUSED;
+        }
+      }
+    }
+    
+    pinMode(i, INPUT_PULLUP);
+  }
+}
