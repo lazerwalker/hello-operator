015a0e36924ade1d26bbb13e63407b418101f2b3
Mike Walker <michael@lazerwalker.com>
2015-08-24 11:29:28 -0700

    Add iOS project

---
+++ ios/Switchboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Switchboard
+//
+//  Created by Mike Lazer-Walker on 8/24/15.
+//  Copyright © 2015 Mike Lazer-Walker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
