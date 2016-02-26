e28558d3c65e0f58e7002f6d17e336619be3ca1f
Mike Walker <michael@lazerwalker.com>
2016-02-25 19:02:01 -0500

    Add CableLights (renamed)

---
+++ arduino/551/cableLights/cableLights.ino
@@ -0,0 +1,27 @@
+
+
+const int LOW_PIN = 3;
+int HIGH_PIN = 52;
+
+
+void setup() {
+  Serial.begin(9600);
+  
+  Serial.println("\"cableLights\"");
+  for (int i=LOW_PIN; i<=HIGH_PIN; i++) {
+    pinMode(i, OUTPUT);
+    digitalWrite(i, HIGH);
+  }
+
+}
+
+void loop() {
+  while (Serial.available() > 0) {
+    int pin = Serial.parseInt();
+    int value = Serial.parseInt();
+    Serial.println(String(pin) + ", " + String(value));
+    if (pin >= LOW_PIN && pin <= HIGH_PIN && (value == 0 || value == 1)) {
+      digitalWrite(pin, value);
+    }
+  }
+}
