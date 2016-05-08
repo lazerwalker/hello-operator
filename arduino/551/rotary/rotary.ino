e9bd4479fd07b0f64b923882d4a2b7163c72adc9
Mike Walker <michael@lazerwalker.com>
2016-05-08 15:27:43 -0400

    Extract out rotary code to its own class

--- arduino/551/rotary/rotary.ino
+++ arduino/551/rotary/rotary.ino
@@ -1,74 +1,20 @@
-#define RINGING_PIN 11
+#define READY_PIN 11
 #define DATA_PIN 12
 
-#define PULSE_RATE 10
-#define NO_NUM -1
+#include "RotaryDial.h"
 
-enum State {
-  WAITING,
-  LISTENING_NOPULSE,
-  LISTENING_PULSE
-};
-
-void changeState(State newState);
-bool changeStateIfDebounced(State newState);
-
-int currentCount;
-State currentState;
-
-unsigned long lastStateChange; // in MS
+RotaryDial rotary(READY_PIN, DATA_PIN);
 
 void setup() {
   Serial.begin(9600);
-
-  pinMode(RINGING_PIN, INPUT_PULLUP);
-  pinMode(DATA_PIN, INPUT_PULLUP);
- 
   Serial.println("Hello World");
 
-  changeState(WAITING);
-  currentCount = 0;
+  rotary.setup();
 }
 
-bool checkDebounce() {
-   return (millis() - lastStateChange) > PULSE_RATE;
-}
-
-bool changeStateIfDebounced(State newState) {
-  if (checkDebounce()) {
-    changeState(newState);
-    return true;
-  }
-  return false;
-}
-
-void changeState(State newState) {
-  currentState = newState;
-  lastStateChange = millis();
-}
 
 void loop() {
-  bool ringingPin = digitalRead(RINGING_PIN);
-  bool dataPin = digitalRead(DATA_PIN);
-  
-  if (currentState == WAITING) {
-    if (ringingPin == HIGH && changeStateIfDebounced(LISTENING_NOPULSE)) {
-      currentCount = 0;
-    }
-  } else if (currentState == LISTENING_NOPULSE) {
-    if (ringingPin == LOW && changeStateIfDebounced(WAITING)) {
-      if (currentCount == 0) {
-        currentCount = NO_NUM;
-      } else if (currentCount == 10) {
-        currentCount = 0;
-      }
-      Serial.println(currentCount);
-    } else if (dataPin == HIGH && changeStateIfDebounced(LISTENING_PULSE)) {
-      currentCount++;
-    } 
-  } else if (currentState == LISTENING_PULSE) {
-    if (dataPin == LOW) {
-      changeStateIfDebounced(LISTENING_NOPULSE);
-    }
+  if (rotary.update()) {
+    Serial.println(rotary.getNumber());
   }
 }
